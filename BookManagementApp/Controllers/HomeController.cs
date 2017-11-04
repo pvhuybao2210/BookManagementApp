c824337e3242be1252c3925e978e1d2b44452fc3
duymi <duymi@DESKTOP-0DSM530>
2017-11-04 22:39:24 +0700

    Modify StocksController to show only latest book quantity

--- BookManagementApp/Controllers/HomeController.cs
+++ BookManagementApp/Controllers/HomeController.cs
@@ -18,109 +18,22 @@ namespace BookManagementApp.Controllers
         // GET: Stocks
         public ActionResult Index()
         {
-            var stocks = db.Stocks.Include(s => s.Book);
-            return View(stocks.ToList());
-        }
+            var stocks = db.Stocks.ToList(); 
+            var books = db.Books.ToList();
+            List<Stock> stockResult = new List<Stock>();
 
-        // GET: Stocks/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            foreach (var b in books)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Stock stock = db.Stocks.Find(id);
-            if (stock == null)
-            {
-                return HttpNotFound();
-            }
-            return View(stock);
-        }
-
-        // GET: Stocks/Create
-        public ActionResult Create()
-        {
-            ViewBag.BookID = new SelectList(db.Books, "ID", "Name");
-            return View();
-        }
-
-        // POST: Stocks/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,BookID,Quantity,Date")] Stock stock)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Stocks.Add(stock);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                Stock stock = stocks.Where(s => s.BookID == b.ID).OrderBy(s => s.Date).Last();
+                stockResult.Add(stock);
             }
 
-            ViewBag.BookID = new SelectList(db.Books, "ID", "Name", stock.BookID);
-            return View(stock);
+            //var stocks = db.Stocks.Include(s => s.Book);
+
+            return View(stockResult.ToList());
         }
 
-        // GET: Stocks/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Stock stock = db.Stocks.Find(id);
-            if (stock == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.BookID = new SelectList(db.Books, "ID", "Name", stock.BookID);
-            return View(stock);
-        }
-
-        // POST: Stocks/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,BookID,Quantity,Date")] Stock stock)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(stock).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.BookID = new SelectList(db.Books, "ID", "Name", stock.BookID);
-            return View(stock);
-        }
-
-        // GET: Stocks/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Stock stock = db.Stocks.Find(id);
-            if (stock == null)
-            {
-                return HttpNotFound();
-            }
-            return View(stock);
-        }
-
-        // POST: Stocks/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Stock stock = db.Stocks.Find(id);
-            db.Stocks.Remove(stock);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)
