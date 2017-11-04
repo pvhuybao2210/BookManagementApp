77adc939a7a13ca6f31fd39912b1119b76acbb17
duymi <duymi@DESKTOP-0DSM530>
2017-11-04 18:17:24 +0700

    Modify AgencyReport*, Invoice*, Receipt*, Report* models. Modify seed. Create Stock, Publisher, Book, Agency controller

---
+++ BookManagementApp/Controllers/AgenciesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookManagementApp.DAL;
+using BookManagementApp.Models;
+
+namespace BookManagementApp.Controllers
+{
+    public class AgenciesController : Controller
+    {
+        private BookContext db = new BookContext();
+
+        // GET: Agencies
+        public ActionResult Index()
+        {
+            return View(db.Agencies.ToList());
+        }
+
+        // GET: Agencies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agencies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Address,Phone,AccountNumber")] Agency agency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agencies.Add(agency);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agency);
+        }
+
+        // GET: Agencies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agency agency = db.Agencies.Find(id);
+            if (agency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agency);
+        }
+
+        // POST: Agencies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Address,Phone,AccountNumber")] Agency agency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agency).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agency);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BookManagementApp/Controllers/PublishersController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookManagementApp.DAL;
+using BookManagementApp.Models;
+
+namespace BookManagementApp.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private BookContext db = new BookContext();
+
+        // GET: Publishers
+        public ActionResult Index()
+        {
+            return View(db.Publishers.ToList());
+        }
+
+        
+        // GET: Publishers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Publishers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Address,Phone,AccountNumber")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Publishers.Add(publisher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(publisher);
+        }
+
+        // GET: Publishers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publisher publisher = db.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publisher);
+        }
+
+        // POST: Publishers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Address,Phone,AccountNumber")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(publisher).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(publisher);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
