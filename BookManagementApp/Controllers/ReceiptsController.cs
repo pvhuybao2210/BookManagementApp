249675525b3d3136112a54d8c48ff6418e91282a
duymi <duymi@DESKTOP-0DSM530>
2017-11-06 11:23:42 +0700

    Create ReceiptController and modify its views

---
+++ BookManagementApp/Controllers/ReceiptsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookManagementApp.DAL;
+using BookManagementApp.Models;
+
+namespace BookManagementApp.Controllers
+{
+    public class ReceiptsController : Controller
+    {
+        private BookContext db = new BookContext();
+
+        // GET: Receipts
+        public ActionResult Index()
+        {
+            var receipts = db.Receipts.Include(r => r.Publisher);
+            return View(receipts.ToList());
+        }
+
+        // GET: Receipts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Receipt receipt = db.Receipts.Include(r => r.Publisher)
+                                         .Include(r => r.ReceiptDetails.Select(q => q.Book))
+                                         .SingleOrDefault(x => x.ID == id);
+            if (receipt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(receipt);
+        }
+
+        // GET: Receipts/Create
+        public ActionResult Create()
+        {
+            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name");
+            return View();
+        }
+
+        // POST: Receipts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,PublisherID,Description")] Receipt receipt)
+        {
+            if (ModelState.IsValid)
+            {
+                receipt.Total = 0;
+                receipt.Status = true;
+                receipt.Date = DateTime.Now;
+
+                db.Receipts.Add(receipt);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", receipt.PublisherID);
+            return View(receipt);
+        }
+
+        // GET: Receipts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Receipt receipt = db.Receipts.Find(id);
+            if (receipt == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", receipt.PublisherID);
+            return View(receipt);
+        }
+
+        // POST: Receipts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Description,Status")] Receipt receipt)
+        {
+            if (ModelState.IsValid)
+            {
+                var updateReceipt =  new Receipt
+                {
+                    ID = receipt.ID,
+                    Description = receipt.Description,
+                    Status = receipt.Status
+                };
+                db.Receipts.Attach(updateReceipt);
+                db.Entry(updateReceipt).Property(x => x.Description).IsModified = true;
+                db.Entry(updateReceipt).Property(x => x.Status).IsModified = true;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", receipt.PublisherID);
+            return View(receipt);
+        }
+
+        // GET: Receipts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Receipt receipt = db.Receipts.Find(id);
+            if (receipt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(receipt);
+        }
+
+        // POST: Receipts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Receipt receipt = db.Receipts.Find(id);
+            db.Receipts.Remove(receipt);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
