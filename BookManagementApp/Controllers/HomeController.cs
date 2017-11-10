332c67962a56b8645a06147a48efcdb026bdcf3b
duymi <duymi@DESKTOP-0DSM530>
2017-11-10 09:06:03 +0700

    Add filter between 2 dates HomeController

--- BookManagementApp/Controllers/HomeController.cs
+++ BookManagementApp/Controllers/HomeController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using BookManagementApp.DAL;
 using BookManagementApp.Models;
@@ -17,79 +15,70 @@ namespace BookManagementApp.Controllers
 
         public ActionResult Index()
         {
-            // if user doesn't choose filter date, get current date by default
-            DateTime filterDate = DateTime.Now;
+            // if user doesn't choose filter date, set default date
+            DateTime fromDate = new DateTime(1995,1,1);
+            DateTime toDate = DateTime.Now;
 
-            if (!string.IsNullOrWhiteSpace(Request.Form["date"]))
+            if (!string.IsNullOrWhiteSpace(Request.Form["toDate"]))
             {
-                var tempDate = Request.Form["date"].ToString();
+                var tempDate = Request.Form["toDate"].ToString();
                 TimeSpan time = new TimeSpan(23, 59, 59);
-                filterDate = DateTime.Parse(tempDate).Add(time);
+                toDate = DateTime.Parse(tempDate).Add(time);
+                // add hour and minute to the end of date
             }
-            ViewBag.filterDate = filterDate;
-
-            // get all debts from all publishers
-            int totalDebt = 0;
-            ViewBag.totalDebt = 0;
-
-            List<Publisher> publishers = db.Publishers.ToList();
-            ViewBag.publishers = publishers;
-
-            List<Debt> debts = db.Debts.ToList();
-            List<Debt> debtResult = new List<Debt>();
-
-            if(debts != null)
-            {
-                foreach (var x in publishers)
-                {
-                    Debt debt = debts
-                   .Where(s => s.PublisherID == x.ID &&
-                   (filterDate - s.Date).TotalMilliseconds >= 0)
-                   .OrderByDescending(s => s.Date)
-                   .FirstOrDefault();
-
-                    if(debt != null)
-                    {
-                        debtResult.Add(debt);
-                        totalDebt += debt.Amount;
-                    } 
-                }
-                ViewBag.totalDebt = totalDebt;
-            }
+            ViewBag.fromDate = fromDate;
+            ViewBag.toDate = toDate;
 
             // get total payments from agencies => revenue
-            int totalAgencyPayment = 0;
+            int totalPayment = 0;
             ViewBag.totalAgencyPayment = 0;
             ViewBag.revenue = 0;
 
-            List<AgencyReport> agencyReports = db.AgencyReports
-                                                .Where(s => DbFunctions.TruncateTime(s.Date) <= filterDate)
-                                                .Include(s => s.AgencyReportDetails)
-                                                .ToList();
+            List<Agency> agencies = db.Agencies.ToList();
+            ViewBag.agencies = agencies;
+   
+            int[] paymentList = new int[100]; int count = 0;
             int totalSellingPrice = 0;
             int totalPurchasePrice = 0;
 
-            if(agencyReports != null)
+            foreach (Agency agency in agencies)
             {
-                foreach (var x in agencyReports)
+                List<AgencyReport> agencyReports = db.AgencyReports
+                                            .Where(
+                                                s => s.AgencyID == agency.ID
+                                                && DbFunctions.TruncateTime(s.Date) >= fromDate
+                                                && DbFunctions.TruncateTime(s.Date) <= toDate)
+                                            .Include(s => s.AgencyReportDetails)
+                                            .ToList();
+                if(agencyReports != null)
                 {
-                    totalAgencyPayment += x.Total;
-
-                    List<AgencyReportDetail> agencyReportDetails = db.AgencyReportDetails
-                                                    .Where(s => s.AgencyReportID == x.ID)
-                                                    .Include(s => s.Book)
-                                                    .ToList();
-                    foreach (AgencyReportDetail y in agencyReportDetails)
+                    foreach (var x in agencyReports)
                     {
-                        totalSellingPrice += (y.Book.SellingPrice * y.Quantity);
-                        totalPurchasePrice += (y.Book.PurchasePrice * y.Quantity);
+                        paymentList[count] += x.Total;
+
+                        totalPayment += x.Total;
+
+                        List<AgencyReportDetail> agencyReportDetails = db.AgencyReportDetails
+                                                        .Where(s => s.AgencyReportID == x.ID)
+                                                        .Include(s => s.Book)
+                                                        .ToList();
+
+                        foreach (AgencyReportDetail y in agencyReportDetails)
+                        {
+                            totalSellingPrice += (y.Book.SellingPrice * y.Quantity);
+                            totalPurchasePrice += (y.Book.PurchasePrice * y.Quantity);
+                        }
                     }
                 }
-                ViewBag.revenue = totalSellingPrice - totalPurchasePrice;
-                ViewBag.totalAgencyPayment = totalAgencyPayment;
+                
+                count++;
             }
-            
-            return View(debtResult);
+
+            ViewBag.paymentList = paymentList;
+            ViewBag.revenue = totalSellingPrice - totalPurchasePrice;
+            ViewBag.totalPayment = totalPayment;
+
+            return View(paymentList);
         }
 
 
